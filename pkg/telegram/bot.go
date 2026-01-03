62f11241891d55d92afffbace4890b462a8b791e
Anthony Ilinykh <anthonyilinykh@gmail.com>
2026-01-03 13:11:11 +0300

    Add `context` to bot API call for requests cancellation

--- pkg/telegram/bot.go
+++ pkg/telegram/bot.go
@@ -1,6 +1,7 @@
 package telegram
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log/slog"
@@ -16,7 +17,8 @@ func NewBot(opts ...func(*BotConfig)) (*Bot, error) {
 	}
 	return &Bot{
 		User:     me,
-		client:   http.DefaultClient,
+		client:   config.client,
+		ctx:      config.ctx,
 		endpoint: config.endpoint,
 		token:    config.token,
 		l:        config.logger.With("username", me.Username),
@@ -26,6 +28,7 @@ func NewBot(opts ...func(*BotConfig)) (*Bot, error) {
 type Bot struct {
 	*User
 	client   *http.Client
+	ctx      context.Context
 	endpoint string
 	token    string
 	l        *slog.Logger
@@ -53,4 +56,39 @@ func getMe(config *BotConfig) (*User, error) {
 	}
 
 	return &r.Result, nil
+}
+
+func (b *Bot) GetUpdates(offset, timeout int64) ([]*Update, error) {
+	urlString := fmt.Sprintf("%s/bot%s/getUpdates?offset=%d&timeout=%d", b.endpoint, b.token, offset, timeout)
+	b.l.Debug("start polling...", "offset", offset, "timeout", timeout)
+
+	var r struct {
+		Ok          bool      `json:"ok"`
+		Description string    `json:"description"`
+		Result      []*Update `json:"result"`
+	}
+	err := b.do("GET", urlString, &r)
+	if err != nil {
+		return nil, err
+	}
+
+	if !r.Ok {
+		return nil, fmt.Errorf("telegram error: %s", r.Description)
+	}
+
+	return r.Result, nil
+}
+
+func (b *Bot) do(method, url string, result any) error {
+	request, err := http.NewRequestWithContext(b.ctx, method, url, nil)
+	if err != nil {
+		return fmt.Errorf("failed to create request: %w", err)
+	}
+	resp, err := b.client.Do(request)
+	if err != nil {
+		return fmt.Errorf("failed to perform request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	return json.NewDecoder(resp.Body).Decode(result)
 }
--- pkg/telegram/bot_config.go
+++ pkg/telegram/bot_config.go
@@ -1,6 +1,7 @@
 package telegram
 
 import (
+	"context"
 	"log/slog"
 	"net/http"
 )
@@ -10,6 +11,7 @@ func NewBotConfig(opts ...func(*BotConfig)) *BotConfig {
 		endpoint: "https://api.telegram.org",
 		token:    "",
 		client:   http.DefaultClient,
+		ctx:      context.Background(),
 		logger:   slog.Default(),
 	}
 
@@ -25,12 +27,25 @@ type BotConfig struct {
 	token    string
 
 	client *http.Client
+	ctx    context.Context
 	logger *slog.Logger
 }
 
 func WithToken(token string) func(*BotConfig) {
 	return func(bc *BotConfig) {
 		bc.token = token
+	}
+}
+
+func WithClient(client *http.Client) func(*BotConfig) {
+	return func(bc *BotConfig) {
+		bc.client = client
+	}
+}
+
+func WithContext(ctx context.Context) func(*BotConfig) {
+	return func(bc *BotConfig) {
+		bc.ctx = ctx
 	}
 }
 
