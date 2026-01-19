eceaa5b546df177b80735d188658ac95badb281d
Anthony Ilinykh <anthonyilinykh@gmail.com>
2026-01-19 23:46:29 +0300

    Youtube errors refactoring

--- pkg/ytdlp/errors.go
+++ pkg/ytdlp/errors.go
@@ -1,6 +1,7 @@
 package ytdlp
 
 import (
+	"fmt"
 	"os/exec"
 	"strings"
 )
@@ -14,7 +15,7 @@ func (err *Error) Error() string {
 	return err.Description
 }
 
-func NewError(err error, data []byte) *Error {
+func NewError(err error, data []byte) error {
 	execErr, ok := err.(*exec.ExitError)
 	if !ok {
 		return &Error{Code: -1, Description: err.Error()}
@@ -25,21 +26,26 @@ func NewError(err error, data []byte) *Error {
 		stdErr = string(data)
 	}
 
-	if strings.Contains(stdErr, "sign in to confirm you’re not a bot") {
-		return &Error{Code: 403, Description: "Sign in to confirm that you're not a bot"}
-	}
-	if strings.Contains(stdErr, "sign in to confirm your age") {
-		return &Error{Code: 403, Description: "Sign in to confirm your age. This video may be inappropriate for some users"}
-	}
-	if strings.Contains(stdErr, "video unavailable") {
-		return &Error{Code: 404, Description: "This video isn't available any more"}
-	}
-	if strings.Contains(stdErr, "HTTP Error 503: Service Unavailable") {
-		return &Error{Code: 503, Description: "Service Unavailable"}
-	}
-	if strings.Contains(stdErr, "private video") {
-		return &Error{Code: 401, Description: "Video unavailable. This video is private"}
+	errors := []struct {
+		code        int
+		substring   string
+		description string
+	}{
+		{403, "sign in to confirm you’re not a bot", "Sign in to confirm that you're not a bot"},
+		{403, "sign in to confirm your age", "Sign in to confirm your age. This video may be inappropriate for some users"},
+		{404, "removed by the uploader", "Video unavailable. This video has been removed by the uploader"},
+		{404, "account associated with this video", "Video unavailable. This video is no longer available because the YouTube account associated with this video has been terminated."},
+		{404, "removed for violating", "This video has been removed for violating YouTube's Terms of Service"},
+		{404, "video unavailable", "This video isn't available any more"},
+		{503, "503: Service Unavailable", "HTTP Error 503: Service Unavailable"},
+		{401, "private video", "Video unavailable. This video is private"},
 	}
 
-	return &Error{Code: -1, Description: err.Error()}
+	for _, e := range errors {
+		if strings.Contains(stdErr, e.substring) {
+			return &Error{Code: e.code, Description: e.description}
+		}
+	}
+
+	return fmt.Errorf("unexpected error: %w", err)
 }
